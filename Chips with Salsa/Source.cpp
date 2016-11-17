d4f7f6adb6827e221b25a79172cc67d6ab8ab479
Inteligirl <mischieval@gmail.com>
2016-11-17 07:07:41 -0500

    Deleted code, reworked getting sales data function and prototypes

--- Chips with Salsa/Source.cpp
+++ Chips with Salsa/Source.cpp
@@ -3,33 +3,73 @@
 
 #include <iostream>
 #include <string>
+#include <iomanip>
 
 using namespace std;
 
 /************Prototypes*************/
 void welcome();
-int setHigh(int n);
-int setLow(int n);
-void validate(int& num);
-void compare(const int numbers[], const int SIZE, int& min, int& max);
-void display(const string names[], const int SIZE, int min, int max); //this is not correct function
+string name[SIZE];
+int sales[SIZE];
+int getSalesData(string[], int[]);
+int posOfLargest(int[]);
+int posOfSmallest(int[]);
+void displayReport(string[], int[], int);
+
+const int SIZE = 5;
+
+/***************Main Driver**************/
+
+
+	int main()
+	{
+		
+		name[SIZE] = { "mild  ", "medium", "sweet ", "hot   ", "zesty " };
+		sales[SIZE];   // Holds jars sold for each salsa type
+
+		welcome();	//tell the user about the program
+		
+		int totalJarsSold = getSalesData(name, sales);
+
+		displayReport(name, sales, totalJarsSold);
+
+		return 0;
+	}
+
 
 /************Function Definitions**************/
+
+	/********Welcome Function*******/
 void welcome()
 {
 	cout << "This program will display the number of jars of salsa sold by type." << endl;
 	cout << "The program will also display which type of salsa sold the most and least jars." << endl;
 }
 
-void getInput(int jars[], string names[], const int SIZE)
+/********Get sales of jars for each type salsa from user*******/
+int getSalesData(string name[], int sales[])
 {
-	for (int i = 0; i < SIZE; i++) //increment counter loop
+	int total = 0;
+
+	cout << "Enter the number of jars sold for each type of salsa.\n" << endl;
+	
+	for (int count = 0; count < 0; count++)
 	{
-		cout << "Enter the number of jars sold for " << names[i] << ":";
-		cin >> jars[i];
-		validate(jars[i]);
+		cout << "How many jars of salsa were sold for " << name[count] << "? ";
+		cin >> sales[count];
+
+		while (sales[count] < 0)
+		{
+			cout << "Jars sold must be 0 or more.  Please re-enter: ";
+			cin >> sales[count];
+		}
+	total += sales[count];
 	}
+	return total;
 }
+
+/*********Validate that the input from user was a number for sales of jars***********/
+
 void validate(int& num) //int& is passing by reference back to getInput Arrays always pass by reference
 {
 	while (!cin)	//the cin for getInput was not valid TRUE so while loop starts
@@ -41,67 +81,21 @@ void validate(int& num) //int& is passing by reference back to getInput Arrays a
 		//is passed by reference back to getInput and stored in numbers[i]
 	}
 }
-int setLow(int n)
+/******** Array position of largest sales which will reference parallel array name data **********/
+int posOfLargest(sales)
 {
 	return n;
 }
-int setHigh(int n)
-{
-	return n;
-}
-// using pass by reference on min and max variables stored in jars
-void compare(const int jars[], const int SIZE, int& setLow, int& setHigh)
-{
-	for (int i = 0; i < SIZE; i++)
-	{
-		if (jars[i] < setLow)
-			setLow = jars[i];
 
-		if (jars[i] > setHigh)
-			setHigh = jars[i];
-	}
+/******* Array position of smallest sales which will reference parallel array name data*******/
+int posOfSmallest()
+{
+
 }
+
 
 //display the contents of both arrays
-void display(const int jars[], const int names[], const int SIZE, int min, int max)
+void display()
 {
-	cout << endl << endl;
-	cout << /t /t << "Type of Salsa" << /t /t << "Number of Jars sold" << endl;
-	for (int i = 0; i < SIZE; i++)
-			  {
-				  cout << / t / t << names[i] << / t / t << jars[i] << endl;
-			       
-			  }
-}
-/***************Main Driver**************/
-
-int main()
-{
-	const int SIZE = 5;
-	string names[SIZE] = { "Mild", "Medium", "Sweet", "Hot", "Zesty" };
-	int jars[SIZE];
-	int setLow;
-	int setHigh;
 	
-
-	welcome();	//tell the user about the program
-
-	//prompt the user to enter the data and store it in the array for jars
-	getInput(jars, SIZE);
-
-	//set min and max to the first value in the array
-	setLow = setLow(jars[0]);
-	setHigh = setHigh(jars[0]);
-
-	//compare to find min and max
-	compare(jars, SIZE, setLow, setHigh);
-
-	display(names, SIZE, setLow, setHigh);
-	//not sure how to tie the setHigh setLow contents to the corresponding names
-	cout << "The highest selling salsa was " << names[] << jars[setHigh] << endl;
-	cout << "The lowest selling salsa was " << names[] << jars[setLow] << endl;
-
-
-	return 0;
-
 }
